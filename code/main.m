0b9aa6a8ee415b43675609723f21255fbd3023d2
QYuQing <139765443+QYuQing@users.noreply.github.com>
2025-05-27 11:33:07 +0800

    Update main.m

--- code/main.m
+++ code/main.m
@@ -3,10 +3,12 @@ MU = 0.005;GAMMA=0.002;
 %% load adjacency matrix
 dataname = 'e';
 cv_type = 'CVD';
-[y,~,~] = loadtabfile(['D:/KronRLS/IDTI-MK-TCMF-main/data/interactions/' dataname '_admat_dgc.txt']);
+[y,~,~] = loadtabfile(['D:' dataname '_admat_dgc.txt']);
 
 seed = 2024;
-nfolds = 5; nruns=20;
+nfolds = 5;
+nruns=20;
+ratio = 5
 
 [n,m] = size(y);
 [crossval_idx] = fun_cross_idx(n,m,nfolds,seed,cv_type);
@@ -23,12 +25,12 @@ for fold=1:nfolds
     ones_index = find(y_test==1);
     zero_index = find(y_test==0);
     ones_index = ones_index(1:num_ones);
-    zero_index = zero_index(1:5*num_ones);
+    zero_index = zero_index(1:ratio*num_ones);
     test_idx = [test_idx(ones_index);test_idx(zero_index)];
     %% kernel
-    k1_paths = {['D:/KronRLS/IDTI-MK-TCMF-main/data/kernels/' dataname '_simmat_proteins_sw-n.txt'],...
-        ['D:/KronRLS/IDTI-MK-TCMF-main/data/kernels/' dataname '_simmat_proteins_go.txt'],...
-        ['D:/KronRLS/IDTI-MK-TCMF-main/data/kernels/' dataname '_simmat_proteins_ppi.txt'],...
+    k1_paths = {['' dataname '_simmat_proteins_sw-n.txt'],...
+        ['D:/' dataname '_simmat_proteins_go.txt'],...
+        ['D:/' dataname '_simmat_proteins_ppi.txt'],...
         };
     K1 = [];
     for i=1:length(k1_paths)
@@ -36,11 +38,8 @@ for fold=1:nfolds
         mat = process_kernel(mat);
         K1(:,:,i) = Knormalized(mat);
     end
-%     K1(:,:,3+1) = kernel_gip(y_train,1,0.5);
-%     K1(:,:,3+2) = Knormalized( kernel_cosine(y_train,1,MU,GAMMA) );
-%     K1(:,:,3+3) = Knormalized( kernel_corr(y_train,1,MU,GAMMA) );
-    k2_paths = {['D:/KronRLS/IDTI-MK-TCMF-main/data/kernels/' dataname '_simmat_drugs_simcomp.txt'],...
-        ['D:/KronRLS/IDTI-MK-TCMF-main/data/kernels/' dataname '_simmat_drugs_sider.txt'],...
+    k2_paths = {['D:/' dataname '_simmat_drugs_simcomp.txt'],...
+        ['D:/' dataname '_simmat_drugs_sider.txt'],...
         };
     K2 = [];
     for i=1:length(k2_paths)
@@ -49,22 +48,18 @@ for fold=1:nfolds
         K2(:,:,i) = Knormalized(mat);
     end
     gamma_fp = 4;
-    load(['D:/KronRLS/IDTI-MK-TCMF-main/data/kernels/' dataname '_Drug_MACCS_fingerprint.mat']);
+    load(['D:/' dataname '_Drug_MACCS_fingerprint.mat']);
     K2(:,:,i+1) = Knormalized( kernel_gip(Drug_MACCS_fingerprint,1, gamma_fp) );
     gamma=0.5;
-%     K2(:,:,3+1) = Knormalized( kernel_gip(y_train,2,gamma) );
-%     K2(:,:,3+2) = Knormalized( kernel_cosine(y_train,2,MU,GAMMA) );
-%     K2(:,:,3+3) = Knormalized( kernel_corr(y_train,2,MU,GAMMA) );
     
-    %% DTI_MOMK
-    lambda = 1;
-    regcoef = 0.0625;
-    sita = 0.125;
-    alpha = 100;
-    r = 80;
-    sparse = 100;
-    maxiter = 2;
-    [pre] = DTI_RME(K1,K2,y_train,sita,alpha,lambda,regcoef,r,sparse);
+    %% DTI_RME
+	lambda_1 = 0.0625;
+    lambda_2 = 1;
+	lambda_3 = 2;
+	lambda_4 = 100;
+	sita = 0.125;
+    k = 80;
+    [pre] = DTI_RME(K1,K2,y_train,sita,lambda_4,lambda_2,lambda_1,k,lambda_3);
     %% evaluate predictions
     yy=y;
     y_true = yy(test_idx);
